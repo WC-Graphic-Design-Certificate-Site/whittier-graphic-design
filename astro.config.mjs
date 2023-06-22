197260f4d7ce9be8d2029d4ec59d3484d4699f7d
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2023-06-22 11:16:31 -0700

    Dev update
    Mobile style fixes, astro scoped css strategy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
+  scopedStyleStrategy: "class",
   integrations: [svelte()],
 });
