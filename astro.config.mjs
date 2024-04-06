75f7805396abd5b83a30cccbb6d309f396754626
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2024-04-05 17:42:00 -0700

    Upgrade to Astro 4
    and update dependencies, swap in html contact form, Astro Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,32 @@
 import { defineConfig } from "astro/config";
+import browserslist from "browserslist";
+import { browserslistToTargets } from "lightningcss";
 
-import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://phenomenal-wisp-d502f4.netlify.app",
-  compressHTML: true,
+  site: "https://design.whittier.edu/",
   scopedStyleStrategy: "class",
-  integrations: [svelte(), sitemap()],
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.sanity.io",
+      },
+    ],
+  },
+  integrations: [sitemap(), svelte()],
+  vite: {
+    css: {
+      transformer: "lightningcss",
+      lightningcss: {
+        targets: browserslistToTargets(browserslist(">= 0.25%")),
+      },
+    },
+    build: {
+      cssMinify: "lightningcss",
+    },
+  },
 });
